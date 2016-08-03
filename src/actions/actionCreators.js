e21ab0aae8da91107ccb298040a6ac353e9122a4
Anton Semenov <anton@anton.local>
2016-08-03 21:35:48 +0500

    deploy

--- src/actions/actionCreators.js
+++
@@ -1,7 +0,0 @@
-import * as actionTypes from '../actions/actionTypes'
-
-export const addContact = () => ({type: actionTypes.ADD_CONTACT})
-
-export const saveContact = (contactData) => ({type: actionTypes.SAVE_CONTACT, contactData})
-
-export const closeContactDetails = () => ({type: actionTypes.CLOSE_CONTACT_DETAILS})
--- src/containers/ContactTableContainer.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router'
-import ContactTable from '../components/contactTable/ContactTable'
-import { getContacts } from '../reducer'
-
-const mapStateToProps = (state, {location}) => ({contacts: getContacts(state, location.query.filter)})
-
-export default withRouter(connect(mapStateToProps)(ContactTable))
