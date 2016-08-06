1ed6cb883402e4d7e0a83b1a477b41b70c93a632
Anton Semenov <anton@anton.local>
2016-08-06 09:50:23 +0500

    add basepath to router

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route } from 'react-router'
 import App from './app/App'
 import AddContactForm from '../containers/AddContactFormContainer'
+import { createHistory, useBasename } from 'history'
+
+const browserHistory = useBasename(createHistory)({
+  basename: '/contact-keeper'
+})
 
 const Root = ({store}) => (
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path='/contact-keeper' component={App}>
+      <Route path='/' component={App}>
         <Route path='addcontact' component={AddContactForm}/>
       </Route>
     </Router>
